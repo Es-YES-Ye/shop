bfe5455f295b8e892205c80211cdc3f9af479e7a
Younsun Choi <younsun0107@gmail.com>
2022-05-12 10:40:45 +0900

    Querydsl & 상품 관리, 검색 / QueryProjection 메인화면

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -1,27 +1,30 @@
 package com.shop.controller;
 
 
-import com.shop.service.ItemService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.ui.Model;
 import com.shop.dto.ItemFormDto;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import com.shop.service.ItemService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
-
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import javax.persistence.EntityNotFoundException;
 
+import com.shop.dto.ItemSearchDto;
+import com.shop.entity.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -95,6 +98,18 @@ public class ItemController {
         return "redirect:/";
     }
 
+    @GetMapping(value = {"/admin/items", "/admin/items/{page}"})
+    public String itemManage(ItemSearchDto itemSearchDto, @PathVariable("page") Optional<Integer> page, Model model){
+
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 3);
+        Page<Item> items = itemService.getAdminItemPage(itemSearchDto, pageable);
+
+        model.addAttribute("items", items);
+        model.addAttribute("itemSearchDto", itemSearchDto);
+        model.addAttribute("maxPage", 5);
+
+        return "item/itemMng";
+    }
 
     @GetMapping(value = "/item/{itemId}")
     public String itemDtl(Model model, @PathVariable("itemId") Long itemId){
